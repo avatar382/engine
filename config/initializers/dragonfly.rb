1e984c5491d1dceebaf60fb6732bd2ea8dc08f29
Manny Rodriguez <manny@7compass.com>
2016-08-20 15:24:39 -0400

    first commit

---
+++ config/initializers/dragonfly.rb
@@ -0,0 +1,22 @@
+# Configure
+Dragonfly.app(:engine).configure do
+  plugin :imagemagick,
+    convert_command:  `which convert`.strip.presence || '/usr/local/bin/convert',
+    identify_command: `which identify`.strip.presence || '/usr/local/bin/identify'
+
+  verify_urls true
+
+  secret 'ae1da124e04b2d8fea37cfb31fbd16438ed671f5c17784a0c77112198064302d'
+
+  url_format '/images/dynamic/:job/:sha/:basename.:ext'
+
+  fetch_file_whitelist /public/
+
+  fetch_url_whitelist /.+/
+end
+
+# Logger
+Dragonfly.logger = Rails.logger
+
+# Mount as middleware
+Rails.application.middleware.use Dragonfly::Middleware, :engine
